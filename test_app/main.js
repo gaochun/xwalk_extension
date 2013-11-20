6174f7af9c1561d16f96faae4f4e457a79a46681
Gao Chun <chun.gao@intel.com>
2013-11-20 14:25:33 +0800

    Add app for test

---
+++ test_app/main.js
@@ -0,0 +1,27 @@
+﻿window.onload = function() {
+  document.getElementById("btn").onclick = function() {
+    /*xwalk.experimental.dialog.showOpenDialog(true, true, "hello", null, "*.png", function(selection) {
+      console.log(selection);
+    });*/
+    /*xwalk.runtime.getAPIVersion(function(md) {
+      console.log(md);
+    });*/
+    try {
+      var d = new Date().toString();
+      echo.echo(d, function(msg) {
+       document.write(msg + "<br>");
+       var expected = d;
+       if (msg === expected) {
+         document.write("Async echo <font color=green>passed</font>.");
+         document.title = "Pass";
+       } else {
+         document.write("Async echo <font color=red>failed</font>.");
+         document.title = "Fail";
+       }
+      });
+    } catch(e) {
+      console.log(e);
+      document.title = "Fail";
+    }
+  }
+}
